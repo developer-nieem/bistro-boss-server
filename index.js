bea331777b6928e672c2804d002961bd7c61b62a
developer-nieem <naimhossenwork@gmail.com>
2023-05-31 00:08:21 +0600

    made user apis

--- index.js
+++ index.js
@@ -25,18 +25,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
 
+    const bistroCollectionUser = client.db("bistroDB").collection("user");
     const bistroCollectionDb = client.db("bistroDB").collection("menu");
     const bistroCollectionReviews = client.db("bistroDB").collection("reviews");
     const bistroCollectionCart = client.db("bistroDB").collection("carts");
+
+
+    // user related apis
+    app.get("/users", async (req, res) => {
+      const result = await bistroCollectionUser.find().toArray();
+      res.send(result);
+    });
+
+
+    app.post('/users', async(req, res) => {
+      const user =  req.body
+      const query = {email : user.email};
+      const exsertingUser = await bistroCollectionUser.findOne(query);
+      if (exsertingUser) {
+      return  res.send({message : "user already exerting"})
+      }
+      const result = await bistroCollectionUser.insertOne(user);
+      res.send(result)
+    })
+
 
     app.get("/menu", async (req, res) => {
       const result = await bistroCollectionDb.find().toArray();
       res.send(result);
     });
+
+
     app.get("/review", async (req, res) => {
       const result = await bistroCollectionReviews.find().toArray();
       res.send(result);
     });
+
 
     app.get('/carts' , async(req , res) => {
       const email =  req.query.email;
@@ -49,11 +73,13 @@ async function run() {
     })
 
 
+
     app.post("/carts", async (req, res) => {
       const item = req.body;
       const result =  await bistroCollectionCart.insertOne(item);
       res.send(result)
     });
+
 
     app.delete("/carts/:id" , async(req, res) => {
       const id = req.params.id;
